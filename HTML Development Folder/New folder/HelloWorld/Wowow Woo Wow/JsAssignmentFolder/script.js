312eece2ea17fff8a1a61b6cc63f2747ed8e929d
Deja <dejamina7@yahoo.com>
2023-04-28 09:23:23 -0500

    First commit

--- HTML Development Folder/New folder/HelloWorld/Wowow Woo Wow/JsAssignmentFolder/script.js
+++
@@ -1,8 +0,0 @@
-alert("it's working!");
-let topSongs= [
-    '2012',
-    'Gotta Be Your Man',
-    'Movie'
-];
-let letters = ['A', 'B', 'C', 'D'];
-let animals = ['shark','whale','tuna','bear','lizard','tortoise'];
